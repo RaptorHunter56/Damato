71e35bcbf43a7717e93102379548c2f4610babcb
MrMaddog <maddog@mr-maddog.com>
2018-11-27 21:15:09 +1300

    updated for settings for server

--- Damato_API/Damato_API/DataBase/Context.cs
+++ Damato_API/Damato_API/DataBase/Context.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,9 +18,24 @@ namespace Damato_API.DataBase
         public DbSet<File> Files { get; set; }
         public DbSet<Tag> Tags { get; set; }
 
+        public static string Password()
+        {
+            try
+            {
+                using (StreamReader r = new StreamReader(@"D:\home\site\wwwroot\bin\DBSettings.json"))
+                {
+                    string json = r.ReadToEnd();
+                    return JsonConvert.DeserializeObject<string>(json, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+                }
+            }
+            catch(Exception e)
+            {
+                return null;
+            } 
+        }
         public DAMContext()
         {
-            this.Database.Connection.ConnectionString = $"Data Source=damatoapidbserver.database.windows.net,1433;Initial Catalog=DamatoAPI_db;Persist Security Info=False;User ID=Damato;Password={ps};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+            this.Database.Connection.ConnectionString = $"Data Source=damatoapidbserver.database.windows.net,1433;Initial Catalog=DamatoAPI_db;Persist Security Info=False;User ID=Damato;Password={Password()};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
         }
     }
 }
